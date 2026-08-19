f084ba4c1efb8077b3b8150ef67517054ff0811f
Hillary Megan Arianto <mgnpqhma@gmail.com>
2026-08-19 23:38:53 +0800

    Level 0+1 completed

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String banner = " ____        _     \n"
                 + "| __ )  ___ | |__  \n"
                 + "|  _ \\ / _ \\| '_ \\ \n"
                 + "| |_) | (_) | |_) |\n"
                 + "|____/ \\___/|_.__/ \n";
+
         System.out.println(banner);
         System.out.println("hello im bob !!");
         System.out.println("how can i help :)");
-        System.out.println("lalalalalalalalalalalalalalala");
-        System.out.println("byebye !! have a good day");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("  yippee glad to have helped (＠＾◡＾)");
+                System.out.println("  byebye !! have a good day (๑˃ᴗ˂)ﻭ");
+                break;
+            }
+
+            System.out.println(input);
+        }
     }
 }
