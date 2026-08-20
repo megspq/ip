f76c70f970a6528a54aab82fdae2247411bdcd60
Hillary Megan Arianto <mgnpqhma@gmail.com>
2026-08-20 11:00:24 +0800

    Updated task completion state functionality
    Bob can now both mark and unmark tasks as done/undone

---
+++ src/main/java/Task.java
@@ -0,0 +1,50 @@
+/**
+ * Represents a task and whether it has been completed.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates a task that is initially not done.
+     *
+     * @param description description of the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Marks this task as completed.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks this task as not completed.
+     */
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the character used to display this task's completion state.
+     *
+     * @return {@code "X"} if done, or a space otherwise
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Returns this task in the format used by Bob's task list.
+     *
+     * @return the status icon followed by the task description
+     */
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
